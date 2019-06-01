194a77d0db793743663f2e64afbafbd09a5fa34a
jreyesr <jreyesr@users.noreply.github.com>
2019-05-31 21:07:53 -0500

    Agregar tour con intro.js y algo de interacción JS

---
+++ js/interaction.js
@@ -0,0 +1,58 @@
+var currentlyActiveRow = -1;
+
+const classes = { // index: [name, hour_of_start, minute_of_start]
+  1: ["Álgebra Lineal", 9, 30],
+  2: ["Cálculo de Una Variable", 19, 10],
+  3: ["Comunicación I", 19, 30],
+};
+
+function minutes_left(target_class) {
+  const d = new Date();
+  return (target_class[1] - d.getHours()) * 60 + (target_class[2] - d.getMinutes());
+}
+
+function refreshProgressBar(minutes_left) {
+  const container = document.querySelector("#progress_bar_container");
+  container.innerHTML = "";
+  let innerBar = document.createElement("div");
+  innerBar.classList.add("progress-bar");
+
+  let percent_of_bar = (1 - minutes_left / 10) * 100;
+  percent_of_bar = Math.max(Math.min(100, percent_of_bar), 0);
+  innerBar.style = "width: " + percent_of_bar + "%";
+  if (percent_of_bar >= 90) {
+    innerBar.classList.add("bg-danger");
+  } else if (percent_of_bar >= 75) {
+    innerBar.classList.add("bg-warning");
+  }
+
+  container.appendChild(innerBar);
+}
+
+function changeTargetClass(index) {
+  let queryStringPrev = 'tr[data-index="' + currentlyActiveRow + '"] > td';
+  let queryStringNext = 'tr[data-index="' + index + '"] > td';
+  if (currentlyActiveRow > -1) {
+    document.querySelector(queryStringPrev).innerHTML = "";
+  }
+  currentlyActiveRow = index;
+  const clickedRow = document.querySelector(queryStringNext);
+  clickedRow.innerHTML = '<i class="material-icons md-48">arrow_right</i></td>';
+
+  document.querySelector("#monitored_class").innerText = '(' + classes[index][0] + ')';
+  const mins_left = minutes_left(classes[index]);
+  document.querySelector("#minutes_left").value = mins_left > 0 ? mins_left + " mins" : "Started";
+
+  refreshProgressBar(mins_left);
+}
+
+function refreshCheckboxes() {
+  for (let c in classes) {
+    let checkbox = document.querySelector('tr[data-index="' + c + '"] input');
+    checkbox.checked = minutes_left(classes[c]) <= 0;
+  }
+}
+refreshCheckboxes();
+window.setInterval(function() {
+  refreshCheckboxes();
+}, 5000); // Update the different "state" checkboxes every 5 secs
--- js/script.js
+++ js/script.js
@@ -9,43 +9,43 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: 'pk.eyJ1IjoiYWF2ZW5kYW4iLCJhIjoiY2p3NnVzdHozMjdxeDQzcXBnYjlwMTRqcyJ9.S00xReWyD9_Eb4B1h-VgIg'
 }).addTo(mymap);
 
-let markers = [
-  [-2.144666, -79.967649, "Administración FIEC"],
-  [-2.145418, -79.966127, "AEFIEC"],
-  [-2.146217, -79.966816, "Sweet&Coffee"],
-  [-2.146728, -79.966915, "Plazoleta Básico"],
-  [-2.148555, -79.967573, "CELEX"],
-  [-2.147574, -79.966958, "Escalera Biblioteca"],
-  [-2.147564, -79.966132, "Biblioteca Central"],
-  [-2.145980, -79.966291, "FEPOL"],
-  [-2.147530, -79.968110, "Paradero FCSH"],
-  [-2.147856, -79.968176, "Paradero CELEX"],
-  [-2.144575, -79.968064, "Paradero FIEC"],
-  [-2.147681, -79.968720, "Administración FCSH"],
-  [-2.147486, -79.967866, "Auditorio FCSH"],
-  [-2.143591, -79.967920, "Gimnasio profesores"],
-  [-2.143048, -79.967154, "UBEP"],
-  [-2.142373, -79.967184, "Coliseo nuevo"],
-  [-2.144991, -79.965246, "Paradero FIMCP"],
-  [-2.144579, -79.966075, "Administración FIMCP"]
-];
+const markers = {
+  1: [-2.144666, -79.967649, "Administración FIEC"],
+  2: [-2.145418, -79.966127, "AEFIEC"],
+  3: [-2.146217, -79.966816, "Sweet&Coffee"],
+  4: [-2.146728, -79.966915, "Plazoleta Básico"],
+  5: [-2.148555, -79.967573, "CELEX"],
+  6: [-2.147574, -79.966958, "Escalera Biblioteca"],
+  7: [-2.147564, -79.966132, "Biblioteca Central"],
+  8: [-2.145980, -79.966291, "FEPOL"],
+  9: [-2.147530, -79.968110, "Paradero FCSH"],
+  10: [-2.147856, -79.968176, "Paradero CELEX"],
+  11: [-2.144575, -79.968064, "Paradero FIEC"],
+  12: [-2.147681, -79.968720, "Administración FCSH"],
+  13: [-2.147486, -79.967866, "Auditorio FCSH"],
+  14: [-2.143591, -79.967920, "Gimnasio profesores"],
+  15: [-2.143048, -79.967154, "UBEP"],
+  16: [-2.142373, -79.967184, "Coliseo nuevo"],
+  17: [-2.144991, -79.965246, "Paradero FIMCP"],
+  18: [-2.144579, -79.966075, "Administración FIMCP"]
+};
 
-let areas = [
-  [-2.144666, -79.967649, "Toda esta Área"],
-]
+const areas = {
+  1: [-2.144666, -79.967649, "Toda esta Área"],
+};
 
-for (let m of markers) {
-  let marker = L.marker([m[0], m[1]]).addTo(mymap);
-  marker.bindPopup(m[2]);
+for (let k in markers) {
+  let marker = L.marker([markers[k][0], markers[k][1]]).addTo(mymap);
+  marker.bindPopup(markers[k][2]);
 }
-for (let area of areas) {
-  let circle = L.circle([area[0], area[1]], {
+for (let k in areas) {
+  let circle = L.circle([areas[k][0], areas[k][1]], {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.2,
     radius: 20
   }).addTo(mymap);
-  circle.bindPopup(area[2]);
+  circle.bindPopup(areas[k][2]);
 }
 
 var myCurrPosMarker = L.circle([0, 0], {
@@ -65,7 +65,6 @@ function geoFindMe() {
   function success(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    console.log(latitude, longitude);
 
     myCurrPosMarker.setLatLng([latitude, longitude]);
     myCurrPosMarkerPrecision.setLatLng([latitude, longitude]);
