3c7d2322b7b3790dc1fc975a92c1a994937171ff
daledeleo <you@example.com>
2019-06-17 11:00:16 -0500

    modificaciones en events.html

--- js/eventos-script.js
+++ js/eventos-script.js
@@ -4,8 +4,9 @@ $(document).ready(function() {
     user = JSON.parse(user);
   } else {
     $("section#mis-eventos ul").hide(); // Hide "Mis eventos" from document
-    $("section#mis-eventos").append("<div class='alert alert-warning' role='alert'>¡Inicie sesión para ver su lista de eventos, añadir nuevos eventos a su lista o poder eliminarlos de su lista!\n<small class='text-muted'><a href='login.html'>Iniciar sesion</a></div>");
-    $("div#all_events a").hide(); //Ocultra los guardar y olvidar en los eventos
+    $("section#mis-eventos").append("<div class='alert alert-warning' role='alert'>¡Inicie sesión para ver su lista de eventos,"+ 
+    "añadir nuevos eventos a su lista o poder eliminarlos de su lista!\n<small class='text-muted'><a href='login.html'>Iniciar sesion</a></div>");
+    $("div#all_events a#p").hide(); //Ocultra los guardar y olvidar en los eventos
     
   }
 });
